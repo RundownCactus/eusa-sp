14fde7f70d6da555cbd60ed463ce77e94d505c49
Salman Qureshi <45896078+RundownCactus@users.noreply.github.com>
2020-10-19 12:10:20 +0500

    Phone Auth added

--- app/src/main/java/com/akashali/eusaserviceprovider/ServiceProviderPhoneVerification.java
+++ app/src/main/java/com/akashali/eusaserviceprovider/ServiceProviderPhoneVerification.java
@@ -1,24 +1,31 @@
 package com.akashali.eusaserviceprovider;
 
-import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-
 import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class ServiceProviderPhoneVerification extends AppCompatActivity {
     MaterialButton sendverificationcode;
+    TextInputEditText phno;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service_provider_phone_verification);
         sendverificationcode=findViewById(R.id.sendverificationcode);
+        phno = findViewById(R.id.verifyphonenumber);
+
         sendverificationcode.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String ph = phno.getText().toString();
+                Log.d("WAH","+92"+ph);
                 Intent intent=new Intent(ServiceProviderPhoneVerification.this,ServiceProviderReceiveVerificationCode.class);
+                intent.putExtra("phno",ph);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/akashali/eusaserviceprovider/ServiceProviderReceiveVerificationCode.java
+++ app/src/main/java/com/akashali/eusaserviceprovider/ServiceProviderReceiveVerificationCode.java
@@ -1,34 +1,104 @@
 package com.akashali.eusaserviceprovider;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.arch.core.executor.TaskExecutor;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.TaskExecutors;
 import com.google.android.material.button.MaterialButton;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
 
 public class ServiceProviderReceiveVerificationCode extends AppCompatActivity {
+    String verCode;
     ImageView verifybackbutton;
     MaterialButton verifycode;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service_provider_receive_verification_code);
+
         verifybackbutton=findViewById(R.id.verifybackbutton);
         verifycode=findViewById(R.id.verifycode);
-        verifycode.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent=new Intent(ServiceProviderReceiveVerificationCode.this,GetInfoStepOne.class);
-                startActivity(intent);
-            }
-        });
+
+        String phno = getIntent().getStringExtra("phno");
+        sendCode(phno);
+
+
+        //   verifycode.setOnClickListener(new View.OnClickListener() {
+        //  @Override
+        //   public void onClick(View view) {
+        //         Intent intent=new Intent(ReceiveVerificationCode.this,BasicSearch.class);
+        //    startActivity(intent);
+        //  }
+        //});
         verifybackbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 onBackPressed();
+            }
+        });
+    }
+
+    private void sendCode(String phno) {
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                "+92" + phno,        // Phone number to verify
+                60,                 // Timeout duration
+                TimeUnit.SECONDS,   // Unit of timeout
+                TaskExecutors.MAIN_THREAD,               // Activity (for callback binding)
+                mCallbacks);        // OnVerificationStateChangedCallbacks
+    }
+
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+        @Override
+        public void onCodeSent(@NonNull String s, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+            super.onCodeSent(s, forceResendingToken);
+            verCode = s;
+        }
+
+        @Override
+        public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+            String code = phoneAuthCredential.getSmsCode();
+            if(code != null){
+                verifyCode(code);
+            }
+        }
+
+        @Override
+        public void onVerificationFailed(@NonNull FirebaseException e) {
+            Toast.makeText(ServiceProviderReceiveVerificationCode.this,e.getMessage(),Toast.LENGTH_SHORT).show();
+        }
+    };
+
+    private void verifyCode(String code) {
+        PhoneAuthCredential cred = PhoneAuthProvider.getCredential(verCode,code);
+        signIn(cred);
+    }
+
+    private void signIn(PhoneAuthCredential cred) {
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        mAuth.signInWithCredential(cred).addOnCompleteListener(ServiceProviderReceiveVerificationCode.this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Intent intent = new Intent(getApplicationContext(),GetInfoStepOne.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                }
             }
         });
     }
