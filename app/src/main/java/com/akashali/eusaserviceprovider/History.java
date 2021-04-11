38020d13426a46422b02855d5ef8798e4519e0ee
Shehroz <i170187@nu.edu.pk>
2021-04-11 21:55:10 +0500

    fixed history backend

--- app/src/main/java/com/akashali/eusaserviceprovider/History.java
+++ app/src/main/java/com/akashali/eusaserviceprovider/History.java
@@ -3,7 +3,7 @@ package com.akashali.eusaserviceprovider;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
-
+//shehroz contribution
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
